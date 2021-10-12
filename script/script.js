9c30f01071834edf2a7bde5f8a7e75da58556b04
HusJAW <hj25691@gmail.com>
2021-10-12 11:35:06 +0100

    form validation

--- script/script.js
+++ script/script.js
@@ -31,3 +31,41 @@ burgerMenu.addEventListener('click', e =>{
 
 hamburgerMenu()
 
+
+
+// basically i need to check if a feild is required 
+// if it is required then on sbmuit and active class will be added to "cant be empty !"
+//cant be empty should be postioend abslout to the input that is required 
+
+// so i have to creat an event listenr that on submit checks if a field is reqired  and its empty then the alert will be activated 
+
+// do the html css to put the alert in place 
+// and set it to display none and add an active class to change it to display block 
+
+function footerRequiredAction(){
+const form = document.querySelector('form')
+const input = document.querySelectorAll('input')
+const alert = document.querySelectorAll('.alert')
+
+
+
+form.addEventListener('submit', e =>{
+    e.preventDefault()
+    let i = 0
+
+    
+    input.forEach( val =>{
+      if(val.value == ""){
+        alert[i].classList.add('alert-on')
+        i++
+      } 
+      else{
+        alert[i].classList.remove('alert-on')
+      i++
+    }
+    
+    })
+   
+})
+}
+footerRequiredAction()
